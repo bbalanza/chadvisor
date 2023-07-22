b1762e455caabf5bb66edd2ba6add64774d82e66
Jose Balanza Martinez <balanza@mailbox.org>
2023-07-22 22:47:29 +0000

    Changing to go-irc-twitch

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/bbalanza/chadvisor
 
 go 1.20
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/gempir/go-twitch-irc/v4 v4.0.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,76 +1,16 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
-	"net"
 	"os"
-	"os/signal"
-	"strings"
-	"syscall"
-	"time"
 
+	twitch "github.com/gempir/go-twitch-irc/v4"
 	godotenv "github.com/joho/godotenv"
 )
 
 const ADDRESS string = "irc.chat.twitch.tv:6667"
 const CONNECTION_TYPE string = "tcp"
-
-type Bot struct {
-	conn net.Conn
-	cred Credentials
-}
-
-type Credentials struct {
-	appID       string
-	accessToken string
-}
-
-func (bot *Bot) Init(connType string, addr string) {
-	bot.GetCredentials()
-	bot.Connect(connType, addr)
-	bot.SetCapabilities()
-	bot.Authenticate()
-}
-
-func (bot Bot) SetCapabilities() {
-
-	err := bot.SendMessage("CAP", "REQ", ":twitch.tv/commands twitch.tv/membership")
-	if err != nil {
-		log.Panicln(err)
-	}
-}
-
-func (bot Bot) SendMessage(messages ...string) error {
-	m := strings.Join(messages, " ")
-	_, err := fmt.Fprintf(bot.conn, "%s\r\n", m)
-	if err != nil {
-		log.Println("Could not write to wire, check your connection plz.")
-		log.Println(err)
-	}
-	return err
-}
-
-func (bot *Bot) Connect(connType string, addr string) {
-	conn, err := net.Dial(connType, addr)
-	if err != nil {
-		log.Panic(err)
-	}
-	conn.SetDeadline(time.Now().Add(5 * time.Second))
-	bot.conn = conn
-	return
-}
-
-func (bot Bot) Disconnect() {
-	err := bot.conn.Close()
-	if err != nil {
-		log.Panicln("Could not disconnect")
-		log.Panic(err)
-	}
-	fmt.Println("Disconnecting")
-	return
-}
 
 func loadEnv() {
 	err := godotenv.Load()
@@ -80,76 +20,9 @@ func loadEnv() {
 	}
 }
 
-func (bot *Bot) GetCredentials() {
-	loadEnv()
-	bot.cred.accessToken = os.Getenv("ACCESS_TOKEN")
-	bot.cred.appID = os.Getenv("APP_ID")
-
-	if bot.cred.appID == "" {
-		log.Panicf("Could not find app ID.")
-	}
-	if bot.cred.accessToken == "" {
-		log.Panicf("Could not find access token.")
-	}
-
-	return
-}
-
-func (bot Bot) Authenticate() {
-
-	if bot.cred == (Credentials{}) {
-		log.Panicln("Bot credentials are not initialized.")
-	}
-
-	if bot.conn == nil {
-		log.Panicln("Bot has no active connection.")
-	}
-
-	err := bot.SendMessage("PASS", "oauth:"+bot.cred.accessToken)
-	if err != nil {
-		log.Panicln("Could not send PASS message.")
-		log.Panic(err)
-	}
-
-	err = bot.SendMessage("NICK", "statheros")
-	if err != nil {
-		log.Panicln("Could not set NICK.")
-		log.Panic(err)
-	}
-
-	return
-
-}
-
-func (bot Bot) ReadResponses() {
-	connReader := bufio.NewReader(bot.conn)
-	message := ""
-	go func() {
-		for {
-			message, _ = connReader.ReadString('\n')
-			fmt.Print(message)
-			time.Sleep(1 * time.Second)
-		}
-	}()
-}
-
 func main() {
 
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	loadEnv()
+  client := twitch.NewClient(os.Getenv(""))
 
-	bot := Bot{}
-
-	go func() {
-		<-c
-		bot.Disconnect()
-		os.Exit(0)
-	}()
-
-	bot.Init(CONNECTION_TYPE, ADDRESS)
-	bot.ReadResponses()
-
-  for {
-    
-  }
 }
