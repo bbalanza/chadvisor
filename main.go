b516ef27d49decaa6e550cf20e75803c16bd1695
Jose Balanza Martinez <balanza@mailbox.org>
2023-07-22 01:14:42 +0000

    Added basic disconnect function

--- main.go
+++ main.go
@@ -8,13 +8,23 @@ import (
 	"syscall"
 )
 
-func makeConnection() net.Conn {
-	conn, err := net.Dial("tcp", "irc://irc.chat.twitch.tv:6697")
+func connect() net.Conn {
+	conn, err := net.Dial("tcp", "irc.chat.twitch.tv:6697")
 	if err != nil {
 		panic(err)
 	}
 
 	return conn
+}
+
+func disconnect(conn net.Conn, s chan os.Signal) {
+  _, err := fmt.Fprintf(conn, "%s\r\n", "QUIT Bye")
+  if err != nil {
+    panic(err)
+  }
+  fmt.Println("Disconnecting")
+  s <- os.Interrupt
+  return
 }
 
 func main() {
@@ -24,11 +34,10 @@ func main() {
 
 	go func() {
 		<-c
-		fmt.Println("Interrupt Recieved!")
 		os.Exit(0)
 	}()
-
-	for {
-		fmt.Println("Running")
-	}
+  
+  conn:= connect()
+  disconnect(conn, c) 
+  
 }
