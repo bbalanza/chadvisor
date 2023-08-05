08c4d870b1b6551768aa32da501ef9481386edd7
Jose Balanza Martinez <balanza@mailbox.org>
2023-08-05 21:48:39 +0000

    Using twitch IRC bot to poll all the messages.

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.20
 require (
 	github.com/gempir/go-twitch-irc/v4 v4.0.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/sashabaranov/go-openai v1.14.1 // indirect
 )
--- main.go
+++ main.go
@@ -4,13 +4,19 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	twitch "github.com/gempir/go-twitch-irc/v4"
 	godotenv "github.com/joho/godotenv"
+	openai "github.com/sashabaranov/go-openai"
 )
 
 const ADDRESS string = "irc.chat.twitch.tv:6667"
 const CONNECTION_TYPE string = "tcp"
+const CHAT_HISTORY_LENGTH = 100
+const CHAT_HISTORY_CAPACITY = CHAT_HISTORY_LENGTH + 10
 
 func loadEnv() {
 	err := godotenv.Load()
@@ -20,28 +26,66 @@ func loadEnv() {
 	}
 }
 
+func setupClient(c *twitch.Client, chat *[]string) {
+
+	c.OnConnect(func() {
+		fmt.Println("Connected...")
+	})
+
+	c.OnSelfJoinMessage(func(message twitch.UserJoinMessage) {
+		fmt.Println("Chatbot Joined!")
+	})
+
+	c.OnPrivateMessage(func(m twitch.PrivateMessage) {
+		fmtMessage := "message: " + m.Message + "\n"
+		*chat = append(*chat, fmtMessage)
+		if len(*chat) >= CHAT_HISTORY_LENGTH {
+			*chat = make([]string, 0, CHAT_HISTORY_CAPACITY)
+		}
+	})
+
+}
+
 func main() {
 
 	loadEnv()
-	client := twitch.NewClient(os.Getenv("NICK"), "oauth:"+os.Getenv("ACCESS_TOKEN"))
 
-	client.Join("projared")
+	var (
+		twitchClient = twitch.NewClient(os.Getenv("NICK"), "oauth:"+os.Getenv("ACCESS_TOKEN"))
+		chat         = make([]string, 0, CHAT_HISTORY_CAPACITY)
+		sigs         = make(chan os.Signal, 1)
+		ticker       = time.NewTicker(5 * time.Second)
+		done         = make(chan int, 1)
+	)
 
-	client.OnConnect(func() {
-		fmt.Println("Connected.")
-	})
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
-  client.OnSelfJoinMessage(func(message twitch.UserJoinMessage){
-    fmt.Println("Chatbot Joined!")
-  })
+	setupClient(twitchClient, &chat)
+	defer func() {
+		ticker.Stop()
+		twitchClient.Disconnect()
+		fmt.Println("Disconnected!")
+	}()
 
-	client.OnPrivateMessage(func(message twitch.PrivateMessage) {
-		fmt.Println(message.User.Name + ": " + message.Message)
-	})
+	go func() {
+		twitchClient.Join("cohhcarnage")
+		err := twitchClient.Connect()
+		if err != nil {
+			log.Panicln(err)
+		}
+	}()
 
-	err := client.Connect()
-	if err != nil {
-		log.Panicln(err)
-	}
+	go func() {
+		for range ticker.C {
+			fmt.Println(chat)
+		}
+	}()
 
+	go func() {
+		<-sigs
+		done <- 1
+	}()
+
+	<-done
+	return
 }
