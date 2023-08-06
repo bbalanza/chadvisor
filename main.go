cb333ccf8d715f962fb1dbd673974fd6febc6e97
Jose Balanza Martinez <balanza@mailbox.org>
2023-08-06 18:38:28 +0000

    Working with the OpenAI api

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"os/signal"
 	"syscall"
 	"time"
-  "context"
 
 	twitch "github.com/gempir/go-twitch-irc/v4"
 	godotenv "github.com/joho/godotenv"
@@ -44,29 +43,35 @@ func setupClient(c *twitch.Client, chat *chat) {
 }
 
 type chat struct {
-	mBuff []twitch.PrivateMessage
-	ch    chan twitch.PrivateMessage
+	ch chan twitch.PrivateMessage
 }
 
-func (chat *chat) summarizeChat(client * openai.Client) (summary string, err error) {
-	messages := make([]string, 0, CHAT_HISTORY_CAPACITY)
-	for _, v := range chat.mBuff {
-		messages = append(messages, v.Message)
+func (chat *chat) summarizeChat(client *openai.Client) (summary string, err error) {
+	messages := ""
+  L:
+	for {
+		select {
+		case m := <-chat.ch:
+			messages = messages + "messages: " + m.Message + "\n"
+		default:
+      break L
+		}
 	}
 
-	req := openai.CompletionRequest{
-		Model:       openai.GPT3TextDavinci003,
-		Prompt:      messages,
-		MaxTokens:   50,
-		Temperature: 0,
-	}
+	// req := openai.CompletionRequest{
+	// 	Model:       openai.GPT3TextDavinci003,
+	// 	Prompt:      messages,
+	// 	MaxTokens:   50,
+	// 	Temperature: 0,
+	// }
 
-  resp, err := client.CreateCompletion(context.Background(), req)
-  if err != nil {
-    return "", err
-  }
+	// resp, err := client.CreateCompletion(context.Background(), req)
+	// if err != nil {
+	//   return "", err
+	// }
 
-  return resp.Choices[0].Text, nil
+	// return resp.Choices[0].Text, nil
+	return messages, nil
 
 }
 
@@ -76,10 +81,9 @@ func main() {
 
 	var (
 		twitchClient = twitch.NewClient(os.Getenv("NICK"), "oauth:"+os.Getenv("ACCESS_TOKEN"))
-		oaiClient = openai.NewClient(os.Getenv("OPEN_AI_TOKEN"))
-		chat = chat{
-			mBuff: make([]twitch.PrivateMessage, 0, CHAT_HISTORY_CAPACITY),
-			ch:    make(chan twitch.PrivateMessage, CHAT_HISTORY_CAPACITY),
+		oaiClient    = openai.NewClient(os.Getenv("OPEN_AI_TOKEN"))
+		chat         = chat{
+			ch: make(chan twitch.PrivateMessage, CHAT_HISTORY_CAPACITY),
 		}
 		sigs   = make(chan os.Signal, 1)
 		ticker = time.NewTicker(5 * time.Second)
@@ -106,25 +110,17 @@ func main() {
 	go func() {
 		for range ticker.C {
 			l := len(chat.ch)
-			if l <= 5 {
-        fmt.Printf("length of chat: %v \n", l)
+			if l <= 20 {
+				fmt.Printf("length of chat: %v \n", l)
 				continue
 			}
 
-			for i := 0; i < l; i++ {
-				select {
-				case m := <-chat.ch:
-					chat.mBuff = append(chat.mBuff, m)
-				default:
-				}
+			summary, err := chat.summarizeChat(oaiClient)
+			if err != nil {
+				log.Printf("Error: %v", err)
+				continue
 			}
-      
-      summary, err := chat.summarizeChat(oaiClient)
-      if err != nil {
-        log.Printf("Error: %v", err)
-        continue
-      }
-      fmt.Println(summary)
+			fmt.Println(summary)
 		}
 	}()
 
