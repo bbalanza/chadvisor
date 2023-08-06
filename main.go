0d91ea740cf56403fc3bef649e18ecc88c7e6a5d
Jose Balanza Martinez <balanza@mailbox.org>
2023-08-06 01:23:54 +0000

    Can make basic requests to OpenAI, need to refactor

--- main.go
+++ main.go
@@ -7,16 +7,17 @@ import (
 	"os/signal"
 	"syscall"
 	"time"
+  "context"
 
 	twitch "github.com/gempir/go-twitch-irc/v4"
 	godotenv "github.com/joho/godotenv"
-  openai "github.com/sashabaranov/go-openai"
+	openai "github.com/sashabaranov/go-openai"
 )
 
 const ADDRESS string = "irc.chat.twitch.tv:6667"
 const CONNECTION_TYPE string = "tcp"
 const CHAT_HISTORY_LENGTH = 100
-const CHAT_HISTORY_CAPACITY = CHAT_HISTORY_LENGTH + 10
+const CHAT_HISTORY_CAPACITY = CHAT_HISTORY_LENGTH + 30
 
 func loadEnv() {
 	err := godotenv.Load()
@@ -26,7 +27,7 @@ func loadEnv() {
 	}
 }
 
-func setupClient(c *twitch.Client, chat *[]string) {
+func setupClient(c *twitch.Client, chat *chat) {
 
 	c.OnConnect(func() {
 		fmt.Println("Connected...")
@@ -37,16 +38,37 @@ func setupClient(c *twitch.Client, chat *[]string) {
 	})
 
 	c.OnPrivateMessage(func(m twitch.PrivateMessage) {
-		fmtMessage := "message: " + m.Message + "\n"
-		*chat = append(*chat, fmtMessage)
-		if len(*chat) >= CHAT_HISTORY_LENGTH {
-			*chat = make([]string, 0, CHAT_HISTORY_CAPACITY)
-		}
+		chat.ch <- m
 	})
 
 }
 
+type chat struct {
+	mBuff []twitch.PrivateMessage
+	ch    chan twitch.PrivateMessage
+}
 
+func (chat *chat) summarizeChat(client * openai.Client) (summary string, err error) {
+	messages := make([]string, 0, CHAT_HISTORY_CAPACITY)
+	for _, v := range chat.mBuff {
+		messages = append(messages, v.Message)
+	}
+
+	req := openai.CompletionRequest{
+		Model:       openai.GPT3TextDavinci003,
+		Prompt:      messages,
+		MaxTokens:   50,
+		Temperature: 0,
+	}
+
+  resp, err := client.CreateCompletion(context.Background(), req)
+  if err != nil {
+    return "", err
+  }
+
+  return resp.Choices[0].Text, nil
+
+}
 
 func main() {
 
@@ -54,8 +76,11 @@ func main() {
 
 	var (
 		twitchClient = twitch.NewClient(os.Getenv("NICK"), "oauth:"+os.Getenv("ACCESS_TOKEN"))
-    openAIClient = openai.NewClient(os.Getenv("OPEN_AI_TOKEN"))
-		chat   = make([]string, 0, CHAT_HISTORY_CAPACITY)
+		oaiClient = openai.NewClient(os.Getenv("OPEN_AI_TOKEN"))
+		chat = chat{
+			mBuff: make([]twitch.PrivateMessage, 0, CHAT_HISTORY_CAPACITY),
+			ch:    make(chan twitch.PrivateMessage, CHAT_HISTORY_CAPACITY),
+		}
 		sigs   = make(chan os.Signal, 1)
 		ticker = time.NewTicker(5 * time.Second)
 		done   = make(chan int, 1)
@@ -71,7 +96,7 @@ func main() {
 	}()
 
 	go func() {
-		twitchClient.Join("cohhcarnage")
+		twitchClient.Join("projared")
 		err := twitchClient.Connect()
 		if err != nil {
 			log.Panicln(err)
@@ -80,7 +105,26 @@ func main() {
 
 	go func() {
 		for range ticker.C {
-			fmt.Println(chat)
+			l := len(chat.ch)
+			if l <= 5 {
+        fmt.Printf("length of chat: %v \n", l)
+				continue
+			}
+
+			for i := 0; i < l; i++ {
+				select {
+				case m := <-chat.ch:
+					chat.mBuff = append(chat.mBuff, m)
+				default:
+				}
+			}
+      
+      summary, err := chat.summarizeChat(oaiClient)
+      if err != nil {
+        log.Printf("Error: %v", err)
+        continue
+      }
+      fmt.Println(summary)
 		}
 	}()
 
